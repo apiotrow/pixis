80ff67ceb6e552bfe1f1090b10865d4f1d22fd58
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2016-06-25 14:19:18 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+	var PIXI = require('./js/lib/pixi.min.js');
+	// create an new instance of a pixi stage
+	var stage = new PIXI.Stage(0x66FF99);
+
+	// create a renderer instance.
+	var renderer = PIXI.autoDetectRenderer(400, 300);
+
+	// add the renderer view element to the DOM
+	document.body.appendChild(renderer.view);
+
+	requestAnimationFrame( animate );
+
+	// create a texture from an image path
+	var texture = PIXI.Texture.fromImage("assets/images/teleportsbehindyou.jpg");
+	// create a new Sprite using the texture
+	var bunny = new PIXI.Sprite(texture);
+
+	// // center the sprites anchor point
+	bunny.anchor.x = 0.5;
+	bunny.anchor.y = 0.5;
+
+	// move the sprite t the center of the screen
+	bunny.position.x = 200;
+	bunny.position.y = 150;
+
+	stage.addChild(bunny);
+
+	function animate() {
+
+	    requestAnimationFrame( animate );
+
+	    // just for fun, lets rotate mr rabbit a little
+	    bunny.rotation += 0.1;
+
+	    // render the stage   
+	    renderer.render(stage);
+	}
+});
